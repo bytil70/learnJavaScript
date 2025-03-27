a393bcd3d86b3c5a3eaac6d3b0f8e130fc63cf69
bytil70 <bytil70@gmail.com>
2025-03-27 12:06:10 +0000

    correction

--- index.js
+++ index.js
@@ -55,6 +55,3 @@ let fullname = 'Bro Code';
 let age = 25;
 let isStudent = false;
 
-document.getElementById("p1").textContent = 'Your name is ${fullname}.';
-document.getElementById("p2").textContent = 'You are ${age} years old.';
-document.getElementById("p3").textContent = 'Enrolled: ${isStudent}';
