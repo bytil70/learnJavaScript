4a30ecfc1877ce6314c76182068fe04af70d11ee
bytil70 <bytil70@gmail.com>
2025-03-27 11:27:14 +0000

    add data

---
+++ index.js
@@ -0,0 +1,60 @@
+/* 1
+console.log('Hello');
+console.log('I like pizza');
+
+window.alert('This is an alert');
+window.alert('I like pizza');
+
+document.getElementById("myH1").textContent = 'Hello';
+document.getElementById("myP").textContent = 'I like pizza';
+
+// This is a comment
+
+/* 
+This
+is
+a
+comment
+*/
+
+/* 2 variable
+let x;
+x = 123;
+console.log(x);
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log(typeof price);
+console.log('You are ${age} years old.')
+console.log('The price is $${price}');
+console.log('Your gpa is: ${gps}')
+
+let firstname = 'Bro';
+let favoritefood = 'pizza';
+let email = 'bro123@gmail.com'
+
+console.log('Your name is ${firstname}');
+console.log('You like ${favoritefood}');
+console.log('Your email is ${email}');
+*/
+
+/*
+let online = false;
+let forsale = true;
+let isStudent = true;
+
+console.log(typeof online);
+console.log('Bro is online: ${online}');
+console.log('Is this car for sale: ${forsale}');
+console.log('Enrolled: ${isStudent}');
+*/
+
+let fullname = 'Bro Code';
+let age = 25;
+let isStudent = false;
+
+document.getElementById("p1").textContent = 'Your name is ${fullname}.';
+document.getElementById("p2").textContent = 'You are ${age} years old.';
+document.getElementById("p3").textContent = 'Enrolled: ${isStudent}';
